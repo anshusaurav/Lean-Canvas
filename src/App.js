cb016e83ca5607f87f1b9532860dca3ae4b0c9b5
anshusaurav <anshu.saurav@gmail.com>
2020-09-11 21:44:14 +0530

    Canvas Container

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
 import React from 'react';
 import './App.scss';
+import CanvasContainer from './components/CanvasContainer';
 
 function App() {
   return (
-    <div className="lean-canvas-container">
-      <div class='a'>
-        <h2>Problem</h2>
-      </div>
-      <div class='b'>
-        <h2>Solution</h2>
-      </div>
-      <div class='c'>
-        <h2>Key Matrics</h2>
-      </div>
-      <div class='d'>
-        <h2>Unique Value Proposition</h2>
-      </div>
-      <div class='e'>
-        <h2>Unfair Advantage</h2>
-      </div>
-      <div class='f'>
-        <h2>Channels</h2>
-      </div>
-      <div class='g'>
-        <h2>Customer Segments</h2>
-      </div>
-      <div class='h'>
-        <h2>Cost Structure</h2>
-      </div>
-
-      <div class='i'>
-        <h2>Revenue Streams</h2>
-      </div>
-
-    </div>
+    <CanvasContainer />
   );
 }
 
---
+++ src/components/CanvasContainer.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+class CanvasContainer extends Component {
+    render() {
+        return (
+            <div className="lean-canvas-container">
+                <div class='a box'>
+                    <h2>Problem</h2>
+                </div>
+                <div class='b box'>
+                    <h2>Solution</h2>
+                </div>
+                <div class='c box'>
+                    <h2>Key Matrics</h2>
+                </div>
+                <div class='d box'>
+                    <h2>Unique Value Proposition</h2>
+                </div>
+                <div class='e box'>
+                    <h2>Unfair Advantage</h2>
+                </div>
+                <div class='f box'>
+                    <h2>Channels</h2>
+                </div>
+                <div class='g box'>
+                    <h2>Customer Segments</h2>
+                </div>
+                <div class='h box'>
+                    <h2>Cost Structure</h2>
+                </div>
+
+                <div class='i box'>
+                    <h2>Revenue Streams</h2>
+                </div>
+
+            </div>
+        )
+    }
+}
+export default CanvasContainer;
